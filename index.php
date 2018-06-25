847e5daad500eb005ebb69c5fa1ea490f54b1b0d
pankaj korde <pankaj@aci.apexcovantage.com>
2018-06-25 17:20:38 +0530

    third time push

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
     <head>
         <meta charset="utf-8">
         <!-- This file has been downloaded from Bootsnipp.com. Enjoy! -->
-        <title>Image Editor pankaj</title>
+        <title>Image Editor pankaj hahaha</title>
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet">
 		<link rel="stylesheet" type="text/css" href="assets/ac_style.css">
